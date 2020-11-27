a106db3b1b139be0099a5a3816409c7266b8d927
shellyyg <chih-ching.yang@whu.edu>
2020-11-27 23:31:28 +0800

    got fox news data

--- getFoxNewsData.py
+++ getFoxNewsData.py
@@ -32,7 +32,63 @@ def listToString(s):
 
 #Find Post Link
 driver = webdriver.Chrome()
-url='https://www.foxnews.com/search-results/search?q=trump, vaccination&submit=Search'
-driver.get(url)
-time.sleep(3)
-driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')  #scroll down
+
+
+def findArticles(n, topic1, topic2):
+    topics = []
+    topics.append(topic1)
+    topics.append(topic2)
+    topicString = listToString(topics)
+
+    url=f'https://www.foxnews.com/search-results/search?q={topic1}, {topic2}&submit=Search'
+    print("url is: "+url)
+    driver.get(url)
+    time.sleep(3)
+
+    for i in range(n): # load n times
+        print(i)
+        try:
+            driver.execute_script('window.scrollTo(0, document.body.scrollHeight);') #scroll down
+            time.sleep(3)
+            driver.find_element_by_class_name('load-more').click()  #click on loadmore
+            time.sleep(3)
+            print("load button has shown again")
+        except:
+            print("no load button")
+
+    postLinks = []
+    postTitles = []
+    postTimes = []
+    postContent = []
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    posts = soup.findAll('article',{'class':'article'})
+
+    for i in posts:
+        postLinks.append(i.find('a').attrs['href'])
+        postTitles.append(i.find('h2',{'class':'title'}).string)
+        postTimes.append(i.find('span',{'class':'time'}).string)
+        postContent.append(i.find('p',{'class':'dek'}).find('a').string)
+
+    Foxdf = pd.DataFrame({'postlink': postLinks,
+                          'published_time':postTimes,
+                          'title':postTitles,
+                          'abstract':postContent})
+    Foxdf["news_id"] = "0"
+    Foxdf["news_source"] = "Fox News"
+    Foxdf["topics"] = topicString
+    Foxdf['SavedDate'] = date.today()
+    print(Foxdf)
+
+    print("Start saving to sql")
+    Foxdf.columns = ['post_link','published_time','title','abstract','news_id', 'news_source','topics','saved_date']
+    Foxdf.to_sql(
+    'news_rawdata',
+    con=engine,
+    index=False,
+    if_exists = 'append'  #if table exist, then append the rows rather than fail (default is fail)
+    )   
+
+    print("Finished saving to sql")
+
+
+findArticles(2, "Trump", "vaccination")
