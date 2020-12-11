2a1ffe54646f9dc20613d70b6763493c221bbdfd
shellyyg <chih-ching.yang@whu.edu>
2020-12-11 15:45:16 +0800

    able to send message from server to front

--- routes/user/socketBack.js
+++ routes/user/socketBack.js
@@ -45,36 +45,13 @@ function getRoomUsers(room){
 // Run when client connects
 let socketCon = function(io){
     io.on('connection', socket => {
-        socket.on('joinRoom', ({ username, room }) => {
-            const user = userJoin(socket.id, username, room);
-            console.log("io.on 3 user",user); //{ id: 'g-ChP6WK0uTkwvtUAAAB', username: 'dd', room: 'Python' }
-            socket.join(user.room);
-    
-        // Welcome current user
-        socket.emit('message',formatMessage(botName,'歡迎來跟寂寞的Shelly talk talk')); 
-    
-        // Broadcast when a user connects
-        socket.broadcast
-            .to(user.room)
-            .emit(
-                'message',
-                formatMessage(botName,`${user.username} 來了`)
-                );
-            // Send users and room info
-            io.to(user.room).emit('roomUsers', {
-                room: user.room,
-                users: getRoomUsers(user.room)
-            });
-        });
+        
+        socket.emit('message','Welcome to PoliticChat!');
+        
     
         // Listen to chatMessage
         socket.on('chatMessage', (msg)=> {
-            console.log("4");
-            const user = getCurrentUser(socket.id);
-            console.log("5", user);
-            console.log("user aka getCurrentUser(socket.id)is:", user);
-            // Emit the listened message to everybody
-            io.to(user.room).emit('message', formatMessage(user.username,msg));
+            console.log("msg Backend: ", msg);
         })
     
         // Broadcast when a user disconnects
