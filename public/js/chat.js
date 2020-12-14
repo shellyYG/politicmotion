948fe838e2c65a0f9b8104b152290892ab248f40
shellyyg <chih-ching.yang@whu.edu>
2020-12-14 21:41:54 +0800

    finish online user

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,17 +5,11 @@ console.log("buddiesToChat (Front): ", buddiesToChat);
 
 let buddyNames = buddiesToChat.map(element=>element.buddies);
 console.log("buddyNames: ", buddyNames);
-
+const chatForm = document.getElementById('chat-form');
+const msgPlaceHolder = document.getElementById('messages');
 const partnerContainer = document.getElementById('partnerContainer');
-for (i=0; i<buddyNames.length; i++){
-    var partnerDiv = document.createElement('div');
-    partnerDiv.innerHTML = `${buddyNames[i]}`;
-    partnerContainer.appendChild(partnerDiv);
-}
-
-
-msgPlaceHolder.appendChild(div);
-
+const startChatBtn = document.getElementById('startChat');
+let selfName;
 
 const socket = io({
     query: {
@@ -23,22 +17,50 @@ const socket = io({
         buddyNames: buddyNames
     }
 });
-const chatForm = document.getElementById('chat-form');
-const msgPlaceHolder = document.getElementById('messages');
-
-
 
 socket.on("connect",() => { //default event from socket.io. Have to write "connect" instead of "connection" (but at back-end you need to write "connection")
     console.log("@Front socket.id: ", socket.id);
 })
-
-// Not permitted user
+// ======================================================== Not permitted user
 socket.on('AuthError', () => {
     alert("Please sign-in to continue.");
     window.location.href="/signIn.html"
 })
 
-// Permitted user
+// ======================================================== Permitted user
+// -- get self
+socket.on('Self', (self)=>{
+    selfName = self
+    
+});
+console.log("selfName: ". selfName);
+// --- get who is online
+socket.on('OnlineUser', (onlineUsers)=>{
+    console.log("online users: ", onlineUsers);
+
+    for (i=0; i<onlineUsers.length; i++){
+        if(onlineUsers[i]!==selfName){
+            var onlineUserDiv = document.createElement('div');
+            onlineUserDiv.setAttribute("id", `buddy_${onlineUsers[i]}`);
+            onlineUserDiv.innerHTML = `${onlineUsers[i]}`;
+            partnerContainer.appendChild(onlineUserDiv);
+        } 
+    }
+})
+
+var buddyButtons = document.querySelectorAll('[id^="buddy_"]');
+for(let i=0; i<buddyButtons.length; i++){
+    let buddyButton = buddyButtons[i];
+    buddyButton.addEventListener('click',()=>{
+        localStorage.setItem('selectedPartner', buddyButton.innerHTML);
+    })
+}
+
+const clickedUser = localStorage.getItem("selectedPartner");
+
+// --- send chat partner to backend
+socket.emit('clickedUser', clickedUser);
+
 chatForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     // Get message inserted by user
@@ -48,16 +70,62 @@ chatForm.addEventListener('submit',(e)=>{
     }
     socket.emit('chatMsg',msg);
 })
-
-socket.on('echoChatMsgFromBack',(msg)=>{
-    const div = document.createElement('div');
-    div.innerHTML = `<li>${msg}</li>`;
-    msgPlaceHolder.appendChild(div);
-
-    console.log("@Front - echoChatMsgFromBack: ", msg);
+socket.on('echoChatMsg',(x)=>{
+    console.log("echoChatMsg: ", x);
 })
 
-// pm part
-socket.on('echoPm',(msg)=>{
-    console.log("@Front - echoPmFromBack: ", msg);
-})
+// startChatBtn.addEventListener('click',()=>{
+//     const clickedUser = localStorage.getItem("selectedPartner");
+//     const socket = io({
+//         query: {
+//             generalToken: localStorage.getItem("generalToken"),
+//             buddyNames: buddyNames
+//         }
+//     });
+    
+//     socket.on("connect",() => { //default event from socket.io. Have to write "connect" instead of "connection" (but at back-end you need to write "connection")
+//         console.log("@Front socket.id: ", socket.id);
+//     })
+    
+//     // ======================================================== Not permitted user
+//     socket.on('AuthError', () => {
+//         alert("Please sign-in to continue.");
+//         window.location.href="/signIn.html"
+//     })
+    
+//     // ======================================================== Permitted user
+//     // --- send chat partner to backend
+//     socket.emit('clickedUser', clickedUser);
+
+//     chatForm.addEventListener('submit',(e)=>{
+//         e.preventDefault();
+//         // Get message inserted by user
+//         let msg = e.target.elements.m.value; // m is the id of the input box
+//         if(!msg){
+//             return false;
+//         }
+//         socket.emit('chatMsg',msg);
+//     })
+    
+//     // socket.on('echoChatMsgFromBack',(msg)=>{
+//     //     const div = document.createElement('div');
+//     //     div.innerHTML = `<li>${msg}</li>`;
+//     //     msgPlaceHolder.appendChild(div);
+    
+//     //     console.log("@Front - echoChatMsgFromBack: ", msg);
+//     // })
+    
+//     // pm part
+//     /////try1
+//     socket.on('echoChatMsg',(x)=>{
+//         console.log("echoChatMsg: ", x);
+//     })
+
+//     // /////try2
+//     // socket.on('echoPm',(msg)=>{
+//     //     console.log("@Front - echoPmFromBack: ", msg);
+//     // })
+    
+// })
+
+
--- routes/user/chatBack.js
+++ routes/user/chatBack.js
@@ -1,19 +1,21 @@
 const jwt = require('jsonwebtoken');
+const { unique } = require('../../models/tfidf');
 
 var userList = {};
+var onlineUserList = [];
+let eachBuddyName;
 
 let socketCon = function(io){
 
     io.on('connection', (socket)=>{
         let token = socket.handshake.query.generalToken;
         let buddyNames = socket.handshake.query.buddyNames;
-        console.log("buddyNames (Back): ", buddyNames);
-        console.log("type of(buddyNames)", typeof(buddyNames));
+        
         try{
-            let eachBuddyName = buddyNames.split(',');
+            eachBuddyName = buddyNames.split(',');
             console.log("eachBuddyName: ", eachBuddyName);
         }catch{
-            let eachBuddyName = [];
+            eachBuddyName = [];
             console.log("eachBuddyName: ", eachBuddyName);
         }
         
@@ -24,26 +26,48 @@ let socketCon = function(io){
                     console.log("Auth Error!");
                 })
             }else{
-                console.log("payload: ", payload);
-                console.log('a user connected - from @Back');
+                // console.log("payload: ", payload, 'a user connected - from @Back');
                 console.log('@Back socket.id: ', socket.id);
-                var userPackage = {};
-                userPackage.username = payload.data.name;
-                userPackage.socketId = socket.id;
                 
-                userList[payload.data.name] = socket;
+                userList[payload.data.name] = socket; //logged-in
 
-                // console.log("userList (IN): ", userList);
-            
-                socket.on('chatMsg', (x)=>{
-                    console.log("chatMsg received from Front - from @Back: ", x);
-                    io.emit('echoChatMsgFromBack', x);
-                })
+                // console.log("userList: ", userList);
+
+                //emit to front-end who is online
+                for (const key in userList) {
+                    console.log("key: ", key, "payload.data.name: ", payload.data.name);
+                    if(eachBuddyName.includes(key) && key !== payload.data.name){ //only push when it's related users && not self
+                    console.log("key is: ", key);    
+                    onlineUserList.push(key);
+                    }
+                }
+                console.log("onlineUserList: ", onlineUserList);
+                onlineUserList = onlineUserList.filter(unique);
+                socket.emit('Self', payload.data.name);
+                socket.emit('OnlineUser', onlineUserList);
+                
+                // socket.on('chatMsg', (x)=>{
+                //     console.log("chatMsg received from Front - from @Back: ", x);
+                //     io.emit('echoChatMsgFromBack', x);
+                // })
             
                 // pm part
                 /////try1
-                var socketAsAUser = userList[payload.data.name];
-                socketAsAUser.emit('sth','sth');
+                socket.on('clickedUser',(clickedUser)=>{
+                    console.log("clickedUser: ", clickedUser);
+                    var socketAsAUser = userList[clickedUser]; //payload.data.name
+                    if(socketAsAUser !== undefined){
+                        socket.on('chatMsg', (x)=>{
+                            console.log("chatMsg received from Front - from @Back: ", x);
+                            socketAsAUser.emit('echoChatMsg',x);
+                        })
+                        
+                    }else{
+                        console.log("partner is not online.");
+                    }
+                    
+                })
+                
 
                 /////try2
                 socket.on('pm', (anotherSocketId, msg)=>{
