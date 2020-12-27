26e5fb1a76b9c4a7bac10c2f8a401ea607958ec2
shellyyg <chih-ching.yang@whu.edu>
2020-12-27 20:54:47 +0800

    restrict email and nickname to be unique

--- routes/user/signUp.js
+++ routes/user/signUp.js
@@ -30,6 +30,7 @@ router.post('/',(req, res, next)=> {
     //-----get the ivString
     let ivString = iv.toString('base64');
     
+    let signature = data.signature;
     let password = data.password;
     let key = process.env.ACCESS_TOKEN_KEY; 
     let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);//first argument is the encryption type, aka ('aes-256-cbc')
@@ -44,74 +45,97 @@ router.post('/',(req, res, next)=> {
     let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
     let decryptedpass = decipher.update(encryptedpass, 'hex', 'utf-8');
     decryptedpass += decipher.final('utf-8');
-    
-    //-------------------------------------------------------------------------------Create new user in DB
-    async function insertUser(){
-        let insertedData = {
-            provider: "native",
-            username: data.name,
-            email: data.email,
-            encryptpass: encryptedpass,
-            ivString: ivString
-            };
-        let sql = 'INSERT INTO user_basic SET ?';
-        let sqlquery = await query(sql, insertedData);
+    //-------------------------------------------------------------------------------Check if user exists
+    async function checkNameExist(){
+        let sql = `SELECT * FROM user_basic WHERE username = '${data.name}'`;
+        let sqlquery = await query(sql);
         return sqlquery;
     }
-
-    async function getLatestUserId(){
-        let insertUserResult = await insertUser();
-        let latestUserId = insertUserResult.insertId;
-        return latestUserId;
+    async function checkEmailExist(){
+        let sql = `SELECT * FROM user_basic WHERE email = '${data.email}'`;
+        let sqlquery = await query(sql);
+        return sqlquery;
     }
-
-    async function getUserRawAttribute(){
-        let userId = await getLatestUserId();
-        console.log("Latest user ID is:", userId);
-        let sqlUserAttri = 
-        `SELECT id, provider, username, email, encryptpass FROM politicmotion.user_basic WHERE id =${userId}`;
-        let userAttribute = await query(sqlUserAttri);
-        return userAttribute;
-    }
-
-    async function createUserObject(){
-        let userRawAttri = await getUserRawAttribute();
-        let userObject = {};
-        userObject['id']= userRawAttri[0]["id"];
-        userObject['provider']= userRawAttri[0]["provider"];
-        userObject['name']= userRawAttri[0]["username"];
-        userObject['email']= userRawAttri[0]["email"];
-        // userObject['picture']= 'http://3.138.56.214'+userRawAttri[0]["picture"];
-        let finalObject = {};
-        let dataObject = {};
-        dataObject["user"] = userObject;
-        finalObject["data"] = dataObject;
-
-        let payloadObject = {};
-        payloadObject["data"]= userObject;
-        //----------------------------------------------------------------Create token to send to frontend
-        //Create user payload (aka data)
-        const payload = payloadObject;
-        const accessToken = generateAccessToken(payload); //get access token
-        
-        dataObject["access_token"] = accessToken;
-
-        // Verify token to get lifetime for token
-        jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-            if (err) return res.send('You are too long away. Please sign in again.');
-            else {
-                console.log("Payload is",payload);
-                dataObject["access_expired"] = payload.exp-payload.iat; //add lifetime to object for printing out
+    async function checkExist(){
+        const existingUsers = await checkNameExist();
+        const existingEmails = await checkEmailExist();
+        if(existingUsers.length > 0){
+            res.send('username already existed!');
+        }else if(existingEmails.length>0){
+            res.send('email already existed!');
+        }else{
+            //-------------------------------------------------------------------------------Create new user in DB
+            async function insertUser(){
+                let insertedData = {
+                    provider: "native",
+                    username: data.name,
+                    email: data.email,
+                    encryptpass: encryptedpass,
+                    ivString: ivString,
+                    signature: signature
+                    };
+                let sql = 'INSERT INTO user_basic SET ?';
+                let sqlquery = await query(sql, insertedData);
+                return sqlquery;
             }
-        })
-        
-        console.log("finalObject from signUp api is:", finalObject);
-        
-        res.json(finalObject);
-        
-    }
 
-    createUserObject();
+            async function getLatestUserId(){
+                let insertUserResult = await insertUser();
+                let latestUserId = insertUserResult.insertId;
+                return latestUserId;
+            }
+
+            async function getUserRawAttribute(){
+                let userId = await getLatestUserId();
+                let sqlUserAttri = 
+                `SELECT id, provider, username, email, encryptpass FROM politicmotion.user_basic WHERE id =${userId}`;
+                let userAttribute = await query(sqlUserAttri);
+                return userAttribute;
+            }
+
+            async function createUserObject(){
+                let userRawAttri = await getUserRawAttribute();
+                let userObject = {};
+                userObject['id']= userRawAttri[0]["id"];
+                userObject['provider']= userRawAttri[0]["provider"];
+                userObject['name']= userRawAttri[0]["username"];
+                userObject['email']= userRawAttri[0]["email"];
+                
+                let finalObject = {};
+                let dataObject = {};
+                dataObject["user"] = userObject;
+                finalObject["data"] = dataObject;
+
+                let payloadObject = {};
+                payloadObject["data"]= userObject;
+                //----------------------------------------------------------------Create token to send to frontend
+                //Create user payload (aka data)
+                const payload = payloadObject;
+                const accessToken = generateAccessToken(payload); //get access token
+                
+                dataObject["access_token"] = accessToken;
+
+                // Verify token to get lifetime for token
+                jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+                    if (err) return res.send('You are too long away. Please sign in again.');
+                    else {
+                        console.log("Payload is",payload);
+                        dataObject["access_expired"] = payload.exp-payload.iat; //add lifetime to object for printing out
+                    }
+                })
+                
+                console.log("finalObject from signUp api is:", finalObject);
+                
+                res.json(finalObject);
+                
+            }
+
+            createUserObject();
+
+        }
+    }
+    checkExist()
+    
 });
 
 module.exports = router; 
