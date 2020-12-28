8db856d56d4110d3ea71bad60d7bfb777c76ffba
shellyyg <chih-ching.yang@whu.edu>
2020-12-28 10:09:14 +0800

    add log in at homepage

--- public/js/indexFront.js
+++ public/js/indexFront.js
@@ -1,4 +1,9 @@
 const discoverBtn = document.getElementById('btn-discover');
+const logInBtn = document.getElementById('home-login');
+
+logInBtn.addEventListener('click',()=>{
+    window.location.href = "/signIn.html";
+})
 
 discoverBtn.addEventListener('click',()=>{
     window.location.href = "/search.html";
