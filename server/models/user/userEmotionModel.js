f80382f30bb2fa9ab81b753cf7f1b5adce8ecbe6
shellyyg <chih-ching.yang@whu.edu>
2021-01-10 13:16:27 +0800

    implement FK on user_emotion table

--- server/models/user/userEmotionModel.js
+++ server/models/user/userEmotionModel.js
@@ -1,6 +1,15 @@
 const { query } = require("../query");
 
+async function findUserId(email){
+    sql = `SELECT id FROM user_basic WHERE email = '${email}' LIMIT 1;`
+    var sqlquery = await query(sql);
+    return sqlquery;
+}
+
 async function insertUserEmotion(firstSearchTopic, secondSearchTopic, payload, avgUserSentiment, avgUserMagnitude){
+    const userId = await findUserId(payload.data.email);
+    const finalUserId = userId[0].id;
+    console.log("finalUserId: ", finalUserId);
     let insertedData = {
         firstSearchTopic: firstSearchTopic,
         secondSearchTopic: secondSearchTopic,
@@ -8,6 +17,7 @@ async function insertUserEmotion(firstSearchTopic, secondSearchTopic, payload, a
         email: payload.data.email,
         user_sentiment_score: avgUserSentiment.toFixed(2),
         user_magnitude_score: avgUserMagnitude.toFixed(2),
+        user_id: finalUserId
     };
     let sql = "INSERT INTO user_emotion SET ?";
     let sqlquery = await query(sql, insertedData);
